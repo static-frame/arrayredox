c9f95ca86c0afd954c7a0bc4ae83c19b1883dd13
Christopher Ariza <ariza@flexatone.com>
2025-07-08 17:48:54 -0700

    cleanup

--- tests/test_first_true_2d.py
+++ tests/test_first_true_2d.py
@@ -10,7 +10,7 @@ def test_first_true_2d_a() -> None:
     assert pos2.tolist() == [0, 1, 2, 0]
 
     pos1 = first_true_2d(a1, 0)
-    import ipdb; ipdb.set_trace()
+    # import ipdb; ipdb.set_trace()
     assert pos1.tolist() == [0, 1, 2, 0, 1]
 
 
